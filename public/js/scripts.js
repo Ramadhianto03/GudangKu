eae99bcc69852343b3ae6a1bdf86bcbc4149cd86
Ramadhianto03 <mdwiramadhianto@gmail.com>
2023-07-21 17:06:11 +0700

    mengganti template

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,10 +1,7 @@
 /*!
-* Start Bootstrap - Business Casual v7.0.9 (https://startbootstrap.com/theme/business-casual)
+* Start Bootstrap - One Page Wonder v6.0.6 (https://startbootstrap.com/theme/one-page-wonder)
 * Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-one-page-wonder/blob/master/LICENSE)
 */
-// Highlights current date on contact page
-window.addEventListener('DOMContentLoaded', event => {
-    const listHoursArray = document.body.querySelectorAll('.list-hours li');
-    listHoursArray[new Date().getDay()].classList.add(('today'));
-})
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
