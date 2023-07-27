ffa3dc52db1725b8847afe678fb1726a4eed5812
utinsitinurfadillah <201220072@unmuhpnk.ac.id>
2023-07-27 17:22:27 +0700

    mengubah resource fb & ig

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -8,7 +8,7 @@ use App\Models\User;
 
 class RegisterController extends Controller
 {
-    public function index () 
+    public function index ()
     {
         return view('register.index', [
             'title' => 'Register',
